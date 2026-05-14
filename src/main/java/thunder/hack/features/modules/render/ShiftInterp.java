7308ceec358c66c6653ddee296caed3d7bb2447b
abrakadam <yehorslyvkin@gmail.com>
2026-05-14 05:47:23 +0200

    ShiftInterp: убраны лишние настройки

--- src/main/java/thunder/hack/features/modules/render/ShiftInterp.java
+++ src/main/java/thunder/hack/features/modules/render/ShiftInterp.java
@@ -1,8 +1,6 @@
 package thunder.hack.features.modules.render;
 
-//From ThunderHack Plus by VFedTerV
 import net.minecraft.entity.player.PlayerEntity;
-import thunder.hack.core.Managers;
 import thunder.hack.features.modules.Module;
 import thunder.hack.setting.Setting;
 
@@ -12,18 +10,13 @@ public class ShiftInterp extends Module {
     }
 
     private final Setting<Boolean> everyone = new Setting<>("Everyone", true);
-    private final Setting<Boolean> enemies = new Setting<>("Enemies", false);
-    private final Setting<Boolean> friends = new Setting<>("Friends", false);
+    private final Setting<Boolean> playersOnly = new Setting<>("Players", true);
     private final Setting<Boolean> self = new Setting<>("Self", false);
 
     public boolean shouldShift(PlayerEntity player) {
         if (player == null) return false;
-
-        if (everyone.getValue()) return true;
-        if (self.getValue() && player == mc.player) return true;
-        if (enemies.getValue() && !Managers.FRIEND.isFriend(player.getName().getString()) && player != mc.player) return true;
-        if (friends.getValue() && Managers.FRIEND.isFriend(player.getName().getString())) return true;
-
-        return false;
+        if (player == mc.player && !self.getValue()) return false;
+        if (playersOnly.getValue() && !(player instanceof PlayerEntity)) return false;
+        return everyone.getValue();
     }
 }
