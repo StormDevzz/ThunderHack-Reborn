c4ad6f5c0eb9c9f97f9b8d65995c280c082dbbcc
abrakadam <yehorslyvkin@gmail.com>
2026-05-17 03:30:40 +0200

    Супер Зов, Потужно, Ня и Мила

--- src/main/java/thunder/hack/injection/MixinPlayerListHud.java
+++ src/main/java/thunder/hack/injection/MixinPlayerListHud.java
@@ -8,10 +8,14 @@ import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import thunder.hack.ThunderHack;
 import thunder.hack.core.manager.client.ModuleManager;
 import thunder.hack.features.modules.client.ClientSettings;
+import thunder.hack.features.modules.misc.ExtraTab;
 
 import java.util.Comparator;
 import java.util.List;
@@ -33,8 +37,20 @@ public class MixinPlayerListHud {
             return;
 
         if (ModuleManager.extraTab.isEnabled())
-            cir.setReturnValue(mc.player.networkHandler.getListedPlayerListEntries().stream().sorted(ENTRY_ORDERING).limit(1000).toList());
+            cir.setReturnValue(mc.player.networkHandler.getListedPlayerListEntries().stream().sorted(ENTRY_ORDERING).limit(ModuleManager.extraTab.size.getValue()).toList());
         else
             cir.setReturnValue(mc.player.networkHandler.getListedPlayerListEntries().stream().sorted(ENTRY_ORDERING).limit(80).toList());
+    }
+
+    @Inject(method = "getPlayerName", at = @At("HEAD"), cancellable = true)
+    private void getPlayerNameHook(PlayerListEntry entry, CallbackInfoReturnable<Text> cir) {
+        if (ModuleManager.extraTab.isEnabled()) {
+            cir.setReturnValue(ExtraTab.getPlayerName(entry));
+        }
+    }
+
+    @ModifyConstant(method = "render", constant = @Constant(intValue = 20))
+    private int changeMaxPlayersPerColumn(int original) {
+        return ModuleManager.extraTab.isEnabled() ? ModuleManager.extraTab.rows.getValue() : original;
     }
 }
